b4ffdc36ecce36a5c72755bc3cfd0bdceca38618
AidanRB <github@linux.contact>
2023-07-13 11:17:38 -0400

    update to multiple drives

--- createStudentDrive.sh
+++ createStudentDrive.sh
@@ -1,37 +1,64 @@
 #!/usr/bin/env bash
 
 # umount drive
-sudo umount /dev/sda*
+umount /dev/sd*
 
 # exit if error
 set -e
 
-# create partitions/filesystems
-parted --script /dev/sda -- \
-    mktable msdos \
-    mkpart primary fat32 2048s 2000MB \
-    mkpart primary ntfs 2000MB -1s
-echo Partitions created.
+# iterate through all /dev/sd* drives but not partitions
+for DRIVE in /dev/sd*; do #parallel
+    if [[ ! $DRIVE =~ [0-9] ]]; then
+        # create partitions/filesystems
+        parted --script $DRIVE -- \
+            mktable msdos \
+            mkpart primary fat32 2048s 2000MB \
+            mkpart primary ntfs 2000MB -1s
+        echo "Partitions created on "$DRIVE"."
 
-mkfs.vfat -n BOOT /dev/sda1
-mkfs.ntfs -f -L IMAGES /dev/sda2
-echo Filesystems created.
+        mkfs.vfat -n BOOT $DRIVE'1'
+        mkfs.ntfs -f -L DATA $DRIVE'2'
+        echo "Filesystems created on "$DRIVE"."
+    fi
+done
 
-# create mountpoints and mount partitions
-mkdir -p /mnt/drive_boot
-mkdir -p /mnt/drive_images
-mount /dev/sda1 /mnt/drive_boot
-mount /dev/sda2 /mnt/drive_images
-echo Partitions mounted.
+# partimage FAT32 partitions
+for DRIVE in /dev/sd*; do
+    if [[ ! $DRIVE =~ [0-9] ]]; then
+        partimage -b restore $DRIVE'1' $(dirname $0)/boot.partimg.000 #parallel
+        echo "Boot partition populated on "$DRIVE"."
+    fi
+done
+
+# create mountpoints and mount data partitions
+for DRIVE in /dev/sd*; do
+    if [[ ! $DRIVE =~ [0-9] ]]; then
+        LETTER=${DRIVE:7:1}
+        mkdir -p '/mnt/sd'$LETTER'_data'
+        mount $DRIVE'2' '/mnt/sd'$LETTER'_data'
+        echo "Mounted "$DRIVE"2."
+    fi
+done
+
+# create directory structure on drives
+for DRIVE in /mnt/sd*; do
+    find $(dirname $0)/images/ -type d | sed "s|^.*images/*|"$DRIVE"/|" | xargs --replace=% mkdir -p "%"
+done
+echo "Directories created."
 
 # copy files onto drive
-rsync -r --info=progress2 --no-i-r "$(dirname $0)/boot/" "/mnt/drive_boot/"
-rsync -r --info=progress2 --no-i-r "$(dirname $0)/images/" "/mnt/drive_images/"
-echo Files copied.
+IFS=$'\n'
+for FILE in $(find $(dirname $0)/images/ -type f); do
+    FILE=$(echo $FILE | sed "s|^.*images/*||")
+    echo Copying file: $FILE
+    cat $(dirname $0)/images/$FILE | eval "tee /mnt/sd{a..$LETTER}_data/\"$FILE\"" > /dev/null
+done
+echo "Files copied."
 
 # unmount drive
-umount /mnt/drive_boot
-umount /mnt/drive_images
-echo Drive unmounted.
+for MOUNTPOINT in /mnt/sd*_data; do
+    umount $MOUNTPOINT #parallel
+done
+echo "All drives unmounted."
 
-echo Drive created successfully!
+echo "All drives created successfully."
