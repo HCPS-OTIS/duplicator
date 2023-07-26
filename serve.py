16eeebd14b54ef6d69721743ff848084179957c4
AidanRB <github@linux.contact>
2023-07-26 15:28:08 -0400

    add getting drives and modes from user

--- serve.py
+++ serve.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python3
 
-import pywebio as io
+import json
+from pywebio import input, output, start_server
+import subprocess
+
+def get_drives():
+    # Json, Drives, complete Path (/dev/sd?), Output columns
+    ps_drives = subprocess.run(['lsblk', '-Jdpo', 'HOTPLUG,TYPE,SIZE,VENDOR,MODEL,NAME'],
+                               stdout=subprocess.PIPE)
+    all_drives = json.loads(ps_drives.stdout)['blockdevices']
+    drives = [drive for drive in all_drives if drive['hotplug'] and drive['type'] == 'disk']
+    for drive in drives:
+        drive['vendor'] = drive['vendor'].strip()
+        del drive['hotplug']
+        del drive['type']
+    return drives
 
 def main():
     # show progressbar for information gathering
-    io.output.put_progressbar('info', label='Gathering information')
+    output.put_markdown('# Drive creator')
 
     # show and select drives (checkboxes)
+    drives_all = get_drives()
+    labels_all = [f"{d['size']}: {d['vendor']} {d['model']} ({d['name']})" for d in drives_all]
+
+    drives_selected = input.checkbox(options=zip(labels_all, drives_all))
+
+    output.put_info('Selected drives: ' + ', '.join([d['name'] for d in drives_selected]))
+
     # show image options (radio)
+    modes_all = [
+        ['Student drive', 'student'],
+        ['Student drive (Clonezilla)', 'clonezilla'],
+        ['Staff drive (Image Assist)', 'staff'],
+        ['Boxlight firmware upgrade', 'boxlight'],
+        ['Ventoy (Boots ISOs, etc)', 'ventoy']
+    ]
+
+    mode = input.radio(options=modes_all)
+
+    output.put_info('Selected mode: ' + mode)
     # for staff, show driver packs (checkboxes)
     # for ventoy, show ISOs (checkboxes)
-    drives = io.input.checkbox(label='Please select drives to image',
-                               options=['sda', 'sdb', 'sdc', 'sdd'], inline=False)
-    io.output.set_progressbar('info', value=.1)
-    io.output.put_info('Selected drives: ' + str(drives))
 
-io.start_server(main, port=8080)
+start_server(main, port=80)
